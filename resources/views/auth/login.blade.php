61ac2207c75bc28fc6b6706c7d3c9944d30e44ef
NuntawatSophakul <31124804+NuntawatSophakul@users.noreply.github.com>
2019-04-26 00:42:53 +0700

    gg

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-<div class="container">
-    <div class="row justify-content-center">
+<div class="content">
+    <div class="card text-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card text-center">{{ __('Login') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
